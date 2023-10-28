ad8cab20e42ae0fe1da40bc9712102d8b8be6740
Gregory Wainer <greg.wainer@gmail.com>
2023-10-28 19:32:57 -0400

    basic writeup of reading imu, super generic

--- src/pru1/barometer.cpp
+++ src/pru1/barometer.cpp
@@ -1,10 +1,38 @@
 //
 // Created by Gregory Wainer on 10/28/23.
 //
+#include <boost/asio.hpp>
 
 namespace pru1 {
     class barometer {
     public:
+        barometer(const std::string& gpio, int baud_rate){
+            readBar(gpio, baud_rate);
+        }
+    private:
+
+        static void readBar(const std::string& gpio, int baud_rate) {
+            boost::asio::io_service io;
+            boost::asio::serial_port serial(io, gpio);
+
+            serial.set_option(boost::asio::serial_port_base::baud_rate());
+
+            // Send Data
+            serial.write_some(boost::asio::buffer("", 2));
+            serial.write_some(boost::asio::buffer("", 6));
+
+            while(serial.is_open()) {
+                // Read Data
+                char buffer[9];
+                size_t bytesRead = serial.read_some(boost::asio::buffer(buffer));
+                if (true) {
+                    // Add Logic to map buffer to object
+
+                    // Discard the remaining x bytes
+                    serial.read_some(boost::asio::buffer(buffer, 5));
+                }
+            }
+        }
 
     };
 }
--- src/pru1/sensors.cpp
+++ src/pru1/sensors.cpp
@@ -8,14 +8,23 @@
 namespace pru1 {
     class sensors {
     public:
-        pru1::barometer bar;
-        pru1::imu imu;
-
         std::string imu_gpio = "";
-        std::string baro_gpio = "";
+        int imu_baud_rate = 115200;
+
+        std::string baro_gpio;
+        int baro_baud_rate = 115200;
+
+
+        pru1::imu imu = pru1::imu(imu_gpio, imu_baud_rate);
+        pru1::barometer bar = pru1::barometer(baro_gpio, baro_baud_rate);
+
+        static void write_to_sys() {
+            printf("%f", pru1::imu_data.x);
+            
+        }
     };
 }
 
 int main(int argc, char *argv[]) {
-
+    pru1::sensors sensors = pru1::sensors();
 }
