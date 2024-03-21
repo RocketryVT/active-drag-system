f2fe171e27c9ebc047f76bb3d880c114cb51e199
Dawsyn Schraiber <dschraiber@vt.edu>
2024-03-20 20:08:45 -0400

    Conversion to Raspberry Pi Pico Build System; Removed Beaglebone specific code; Simple blinking example in ADS source file; builds for Pico W

--- Vagrantfile
+++ Vagrantfile
@@ -5,14 +5,7 @@ apt-get update && apt-get upgrade
 
 # install base development tools
 apt-get -y install build-essential
-apt-get -y install cmake valgrind
-apt-get -y install crossbuild-essential-armhf
-apt-get -y install libboost-all-dev
-apt-get -y install libasio-dev
-wget https://musl.cc/arm-linux-musleabihf-cross.tgz
-tar -xzvf arm-linux-musleabihf-cross.tgz
-echo 'export PATH="\$PATH:/home/vagrant/arm-linux-musleabihf-cross/bin"' >> .bashrc
-rm -r arm-linux-musleabihf-cross.tgz
+apt-get -y install cmake valgrind gcc-arm-none-eabi
 
 BOOTSTRAP
 #-----------------------------------------------------------------------
--- src/active_drag_system.cpp
+++ src/active_drag_system.cpp
@@ -1,14 +1,22 @@
-#include <iostream>
-#include <cstdlib>
-#include "../include/surfaceFitModel.hpp"
-#include "../include/actuationPlan.hpp"
-#include "../include/ads.hpp"
+/**
+ * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include "pico/stdlib.h"
+#include "pico/cyw43_arch.h"
 
 int main() {
-    
-    SurfaceFitModel sfm = SurfaceFitModel();
-    ActuationPlan plan = ActuationPlan(sfm);
-    ADS ads = ADS(plan);
-    ads.run();
-    return EXIT_SUCCESS;
+    stdio_init_all();
+    if (cyw43_arch_init()) {
+        printf("Wi-Fi init failed");
+        return -1;
+    }
+    while (true) {
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+        sleep_ms(250);
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
+        sleep_ms(250);
+    }
 }
