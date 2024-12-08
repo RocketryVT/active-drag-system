cd11c5bc9e48d14b25d6ccd7c38dc6caa98addec
J. Cian Capacci <cian.capacci@gmail.com>
2024-12-07 19:54:59 -0500

    Reworked altimeter driver, needs actual altitude calculation from converted pressure

--- include/altimeter.hpp
+++ include/altimeter.hpp
@@ -3,29 +3,87 @@
 #include "pico/stdlib.h"
 #include "hardware/i2c.h"
 #include "hardware/gpio.h"
-#include <cstdint>
 
-class altimeter {
-    private:
-        uint8_t altitude_buffer[4];
-        uint8_t buffer[4];
-        uint8_t addr;
-        i2c_inst_t* inst;
-    public:
-        altimeter(i2c_inst_t* inst, uint8_t addr);
+#define BARO_I2C_ADDR (0x77)
 
-        void initialize();
+#define LOW_TEMP_THRESHOLD 20	//Temperature in C below which compensation is needed in digital conversion
 
-        void initialize(float threshold_altitude, uint8_t interrupt_pin, gpio_irq_callback_t callback);
-
-        void set_threshold_altitude(float threshold_altitude, uint8_t interrupt_pin, gpio_irq_callback_t callback);
-
-        void unset_threshold_altitude(uint8_t interrupt_pin);
-
-        float get_altitude_converted();
-
-        void get_altitude_raw(uint8_t* buffer);
-
-        uint32_t expose_buffer(uint8_t** buffer);
+/*General Commands*/
+enum {
+	BARO_RESET = (0x1E),
+	BARO_ADC_READ = (0x00)
 };
 
+/*PROM Read Commands*/
+enum {
+	BARO_PROM_READ_START = (0xA0),
+	BARO_PROM_READ_C1 = (0xA2),
+	BARO_PROM_READ_C2 = (0xA4),
+	BARO_PROM_READ_C3 = (0xA6),
+	BARO_PROM_READ_C4 = (0xA8),
+	BARO_PROM_READ_C5 = (0xAA),
+	BARO_PROM_READ_C6 = (0xAC),
+	BARO_PROM_READ_SERIAL_CRC = (0xAE)
+};
+
+/*Conversion Request Commands (number is OSR)*/
+enum {
+	BARO_CONVERT_PRES_256 = (0x40),
+	BARO_CONVERT_PRES_512 = (0x42),
+	BARO_CONVERT_PRES_1024 = (0x44),
+	BARO_CONVERT_PRES_2048 = (0x46),
+	BARO_CONVERT_PRES_4096 = (0x48),
+	BARO_CONVERT_TEMP_256 = (0x50),
+	BARO_CONVERT_TEMP_512 = (0x52),
+	BARO_CONVERT_TEMP_1024 = (0x54),
+	BARO_CONVERT_TEMP_2048 = (0x56),
+	BARO_CONVERT_TEMP_4096 = (0x58)
+};
+
+class altimeter {
+	private:
+		i2c_inst_t* inst;
+		
+		//Calibration coefficients, loaded during sensor initialization
+		uint16_t c1;	//Pressure Sensitivity
+		uint16_t c2;	//Pressure Offset
+		uint16_t c3;	//Temperature Coefficient of Pressure Sensitivity
+		uint16_t c4;	//Temperature Coefficient of Pressure Offset
+		uint16_t c5;	//Reference Temperature
+		uint16_t c6;	//Temperature Coefficient of the Temperature (???)
+		
+		//Storage for values used in onboard conversion math
+		// Used for decoupling conversion request commands from control loop
+		uint32_t d1;	//Digital Pressure Value
+		uint32_t d2;	//Digital Temperature Value
+		int32_t dt;		//Difference from actual and reference temperature (c5)
+		int32_t dTem;	//Digital temperature
+		int64_t off;	//Temperature offset for pressure
+		int64_t sens;	//Sensitivity at temperature
+
+		//Storage for the actual floating point data, public getters available
+		float altitude;
+		float pressure;
+		float temperature;
+		
+		//State values for conversion math
+		bool compensatingTemperature;
+
+		//Methods for dealing with sensor reading procedure
+ 		uint32_t getADC();	
+	    void calculateAltitude();
+	    void calculateTemperature();
+	    void requestPressureConversion();
+
+	public: 
+		altimeter(i2c_inst_t* inst);
+
+		void initialize();
+		
+		float getTemperature();
+		float getPressure();
+		float getAltitude();
+		
+		bool isFreshAltAvailable();
+		void forceUpdateTemperature();		
+};
--- src/altimeter.cpp
+++ src/altimeter.cpp
@@ -1,145 +1,172 @@
 #include "altimeter.hpp"
-#include "hardware/gpio.h"
 
-altimeter::altimeter(i2c_inst_t* inst, uint8_t addr) {
-    this->inst = inst;
-    this->addr = addr;
+//Default constructor, pass I2C instance
+altimeter::altimeter(i2c_inst_t* inst) {
+	this->inst = inst;
 }
 
-void altimeter::initialize() {
-    // Select control register(0x26)
-    // Active mode, OSR = 16, altimeter mode(0xB8)
-    this->buffer[0] = 0x26;
-    this->buffer[1] = 0x89;
-    i2c_write_blocking(this->inst, this->addr, this->buffer, 2, true);
+bool altimeter::initialize() {
+	//There are literally only 5 commands for this sensor, no write commands beyond the register are needed
+	uint8_t command;
+
+	// Send reset command to sensor
+	command = BARO_RESET;
+	i2c_write_blocking(this->inst, I2C_ADDR, &command, 1, true);	
+	
+	//Read PROM values into memory - 8x16bit registers
+	command = BARO_PROM_READ_START;
+	uint8_t promBuffer[16];	
+	i2c_write_blocking(this->inst, BARO_I2C_ADDR, &command, 1, true);
+	i2c_read_blocking(this->inst, BARO_I2C_ADDR, &command, 16, false);
+	
+	//Check factory ID to verify that values made it through successfully
+	uint16_t promBuffer[0] | promBuffer[1];
+	//TODO: The datasheet doesn't actually say what the "factory data" is,
+	// we'll have to confirm it experimentally for this status check
+
+	//Load calibration constants into memory for conversions
+	c1 = this->promBuffer[2] << 8 | this->promBuffer[3];
+	c2 = this->promBuffer[4] << 8 | this->promBuffer[5];
+	c3 = this->promBuffer[6] << 8 | this->promBuffer[7];
+	c4 = this->promBuffer[8] << 8 | this->promBuffer[9];
+	c5 = this->promBuffer[10] << 8 | this->promBuffer[11];
+	c6 = this->promBuffer[12] << 8 | this->promBuffer[13];	
+
+	//TODO: Load CRC, need to figure out how this works
+	
+	//Read temperature value and use it to set whether temperature compensation is needed
+	//Note: This WILL lock the bus with read requests until temperature is done converting
+	forceUpdateTemperature();
+
+	//TODO: Figure out control flow of feeding asynchronous data to filter, for now start a conversion on startup
+	requestPressureConversion();
+	//Assume sensor is being pinged by the filter every cycle for isFreshAltAvailable() after this point
 }
 
-void altimeter::initialize(float threshold_altitude, uint8_t interrupt_pin, gpio_irq_callback_t callback) {
-    this->initialize();
+//Check that sensor is reporting correctly to the spec
+bool altimeter::validate() {
+	// Read factory data and confirm its validity
+	uint8_t command = BARO_PROM_READ_START;
+	uint8_t readBuffer[2];
+	i2c_write_blocking(this->inst, I2C_ADDR, &command, 1, true);
+	i2c_read_blocking(this->inst, I2C_ADDR, readBuffer, 2, false);
 
-    // Below configures the interrupt for the first transition from PAD to BOOST
-    // Initial Reading
+	//TODO: See initialize(), "Factory Data" is not on datasheet
+	return true;
 
-    sleep_ms(1000);
+	//TODO: Implement CRC validation as well for data returns
 
-    float altitude = 0.0f;
-
-    while (altitude == 0.0f) {
-        altitude = this->get_altitude_converted();
-    }
-
-    threshold_altitude += altitude; // 30 meters above ground
-
-    // Select control register 3 (0x28)
-    // Set bot interrupt pins to active low and enable internal pullups
-    this->buffer[0] = 0x28;
-    this->buffer[1] = 0x01;
-    i2c_write_blocking(this->inst, this->addr, this->buffer, 2, true);
-
-    // Select pressure target MSB register(0x16)
-    // Set altitude target to 30 meters above ground altitude
-    this->buffer[0] = 0x16;
-    this->buffer[1] = (uint8_t) (((int16_t)(threshold_altitude)) >> 8);
-    i2c_write_blocking(this->inst, this->addr, this->buffer, 2, true);
-
-    // Select pressure target LSB register(0x17)
-    // Set altitude target to 30 meters above ground altitude
-    this->buffer[0] = 0x17;
-    this->buffer[1] = (uint8_t) (((int16_t)(threshold_altitude)));
-    i2c_write_blocking(this->inst, this->addr, this->buffer, 2, true);
-
-    // Select interrupt enable register (0x29)
-    // Set interrupt enabled for altitude threshold(0x08)
-    this->buffer[0] = 0x29;
-    this->buffer[1] = 0x08;
-    i2c_write_blocking(this->inst, this->addr, this->buffer, 2, true);
-
-    // Select interrupt this->bufferuration register (0x2A)
-    // Set interrupt enabled for altitude threshold to route to INT1 pin(0x08)
-    this->buffer[0] = 0x2A;
-    this->buffer[1] = 0x08;
-    i2c_write_blocking(this->inst, this->addr, this->buffer, 2, true);
-
-    gpio_set_irq_enabled_with_callback(interrupt_pin, GPIO_IRQ_LEVEL_LOW, true, callback);
-    // End of configuration of interrupt for first transition from PAD to BOOST
 }
 
-void altimeter::set_threshold_altitude(float threshold_altitude, uint8_t interrupt_pin, gpio_irq_callback_t callback) {
-    float altitude = 0.0f;
-
-    while (altitude == 0.0f) {
-        altitude = get_altitude_converted();
-    }
-
-    threshold_altitude += altitude;
-
-    // Select control register 3 (0x28)
-    // Set bot interrupt pins to active low and enable internal pullups
-    this->buffer[0] = 0x28;
-    this->buffer[1] = 0x01;
-    i2c_write_blocking(this->inst, this->addr, this->buffer, 2, true);
-
-    // Select pressure target MSB register(0x16)
-    // Set altitude target to 30 meters above ground altitude
-    this->buffer[0] = 0x16;
-    this->buffer[1] = (uint8_t) (((int16_t)(threshold_altitude)) >> 8);
-    i2c_write_blocking(this->inst, this->addr, this->buffer, 2, true);
-
-    // Select pressure target LSB register(0x17)
-    // Set altitude target to provided threshold altitude
-    this->buffer[0] = 0x17;
-    this->buffer[1] = (uint8_t) (((int16_t)(threshold_altitude)));
-    i2c_write_blocking(this->inst, this->addr, this->buffer, 2, true);
-
-    // Select interrupt enable register (0x29)
-    // Set interrupt enabled for altitude threshold(0x08)
-    this->buffer[0] = 0x29;
-    this->buffer[1] = 0x08;
-    i2c_write_blocking(this->inst, this->addr, this->buffer, 2, true);
-
-    // Select interrupt this->bufferuration register (0x2A)
-    // Set interrupt enabled for altitude threshold to route to INT1 pin(0x08)
-    this->buffer[0] = 0x2A;
-    this->buffer[1] = 0x08;
-    i2c_write_blocking(this->inst, this->addr, this->buffer, 2, true);
-
-    gpio_set_irq_enabled_with_callback(interrupt_pin, GPIO_IRQ_LEVEL_LOW, true, callback);
-    // End of configuration of interrupt for first transition from PAD to BOOST
+//Return altitude (m) as a float 
+float altimeter::getAltitude() {
+	return altitude;
 }
 
-void altimeter::unset_threshold_altitude(uint8_t interrupt_pin) {
-    gpio_set_irq_enabled_with_callback(interrupt_pin, GPIO_IRQ_LEVEL_LOW, false, NULL);
-
-    // Select interrupt enable register (0x29)
-    // Set interrupt enabled for altitude threshold(0x08)
-    this->buffer[0] = 0x29;
-    this->buffer[1] = 0x00;
-    i2c_write_blocking(this->inst, this->addr, this->buffer, 2, true);
-
-    // Select interrupt configuration register (0x2A)
-    // Set interrupt enabled for altitude threshold to route to INT1 pin(0x08)
-    this->buffer[0] = 0x2A;
-    this->buffer[1] = 0x00;
-    i2c_write_blocking(this->inst, this->addr, this->buffer, 2, true);
+//Return the pressure value (mBar) as a float 
+float altimeter::getPressure() {
+	return pressure;
 }
 
-float altimeter::get_altitude_converted() {
-    uint8_t reg = 0x01;
-    i2c_write_blocking(this->inst, this->addr, &reg, 1, true);
-    i2c_read_blocking(this->inst, this->addr, this->altitude_buffer, 4, false);
-    // Exactly how MPL3115A2 datasheet says to retrieve altitude
-    float altitude = (float) ((int16_t) ((this->altitude_buffer[0] << 8) | this->altitude_buffer[1])) + (float) (this->altitude_buffer[2] >> 4) * 0.0625;
-    return altitude;
+//Return the temperature (C) as a float
+float altimeter::getTemperature() {
+	return temperature;
 }
 
-void altimeter::get_altitude_raw(uint8_t* buffer) {
-    uint8_t reg = 0x01;
-    i2c_write_blocking(this->inst, this->addr, &reg, 1, true);
-    i2c_read_blocking(this->inst, this->addr, buffer, 3, false);
+//Request a new pressure (d1) ADC conversion at configured OSR
+void altimeter::requestPressureConversion() {
+	uint8_t command = BARO_CONVERT_PRES_256;	//OSR 256
+	i2c_write_blocking(this->inst, BARO_I2C_ADDR, &command, 1, false);
+}	
+
+//Check conversion status of ADC register, calculate altitude and request a new conversion if data freshly available
+bool isFreshAltAvailable() {
+	//Assume that pressure conversion is underway, shouldn't ever fetch temperature based on driver implementation
+	uint32_t readValue = getADC();
+	if (readValue == 0) {
+		//No new altitude is available
+		return false;
+	} else {
+		d1 = readValue;	//Pass measured conversion to internal variable
+		calculateAltitude();	//Calculate new altitude with digital measurement and temperature offsets
+
+		//New altitude is available for filtering
+		return true;
+	}
 }
 
-uint32_t altimeter::expose_buffer(uint8_t** buffer) {
-    *buffer = this->altitude_buffer;
-    return sizeof(this->altitude_buffer);
+//Take D1 value in memory and use it to compute altitude and pressure, then store them
+void calculateAltitude() {
+	//Calculate constants used for pressure computation
+	off = (c2 << 16) + ((c4 * dT) >> 7);	//Update pressure offset from temperature
+	sens = (c1 << 15) + ((c3 * dT) >> 8);	//Update pressure sensitivity from temperature
+
+	//Add offsets to the above if low temperature compensation is enabled
+	if (compensatingTemperature) {
+		off -= (5 * std::pow(dTem - 2000, 2)) >> 1;
+		sens -= (5 * std::pow(dTem - 2000, 2)) >> 2;
+	}
+
+	//Calculate pressure using the freshly calculated temperature offsets and stored d1
+	int32_t p = (((d1 * sens) >> 21) - off) >> 15;
+
+	//Divide the integer value down two decimal places and store 
+	pressure = (float)p / 100.0f;	//Integer pressure has 2-decimal precision as LSD and LSD+1
+
+	//TODO: Update to add altitude calculation, this will need its own input sea level pressure
 }
+
+//Take D2 value in memory and use it to compute temperature value, then store it
+void calculateTemperature() {
+	//Calculate the temperature using formula from datasheet
+	dt = d2 - (c5 << 8);			//Update dT
+	dTem = (2000 + dT * c6) >> 23;	//Use dT to calculate int temp
+
+	//Use digital temp value to set flag for whether temperature compensation is needed
+	compensatingTemperature = (dTem / 100 < LOW_TEMP_THRESHOLD) ? true : false;
+
+	//With flag set, do compensation if necessary
+	if (compensatingTemperature) dTem -= (dT >> 31);
+	
+	//Convert the integer temperature to float temperature, and update class variable
+	temperature = (float)temp / 100.0f;		//Integer temp has 2-decimal precision as LSD and LSD+1
+}
+
+//Request and return raw ADC value, if output is 0 data is not ready/requested
+uint32_t altimeter::getADC() {
+	//Run ADC read command
+	uint8_t command = BARO_ADC_READ;
+	uint8_t readBuffer[4];						//32-bit digital output value
+	i2c_write_blocking(this->i2c, BARO_I2C_ADDR, &command, 1, true);
+	i2c_read_blocking(this->i2c, BARO_I2C_ADDR, readBuffer, 4, false);	
+	
+	//Return combined 32 bit value
+	return readBuffer[3] << 24 | readBuffer[2] << 16 | readBuffer[1] << 8 | readBuffer[0];
+}
+//Request temperature and block thread until it is complete, and update temperature compensation flag
+void forceUpdateTemperature() {
+	uint8_t command;
+
+	//Write a temperature conversion request
+	command = BARO_COVERT_TEMP_4096;	//Largest OSR
+	uint8_t readBuffer[4];				//32-bit temperature value
+	i2c_write_blocking(this->inst, BARO_I2C_ADDR, &command, 1, true);
+	i2c_read_blocking(this->inst, BARO_I2C_ADDR, &command, 4, false);
+
+	//Run the ADC read command until temperature is available to be converted
+	uint32_t tempD1 = 0;
+	while (tempD1 == 0) {
+		tempD1 = getADC();
+		//TODO: Add reasonable delay to avoid saturating the I2C bus or confusing sensor
+	}
+	d1 = tempD1;
+
+	//Calculate new temperature value
+	calculateTemperature();
+
+	//TODO: As this is intended to be run only occasionally, maybe add some simple in-driver filtering?
+	// Alternative is periodically running a temperature refresh as well, needs testing for drift w/low temp
+}
+
+
 
