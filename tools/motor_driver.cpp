c47c0956c4571201c3bd6a13b15fc5c4a9260715
Dawsyn Schraiber <dschraiber@vt.edu>
2025-02-08 23:06:31 -0500

    Motor goes brrrrr

--- tools/motor_driver.cpp
+++ tools/motor_driver.cpp
@@ -3,9 +3,12 @@
 #include "pico/multicore.h"
 #include "hardware/spi.h"
 #include "hardware/gpio.h"
+#include "hardware/pwm.h"
+#include "hardware/clocks.h"
 #include "boards/pico.h"
 #include "pico/stdio.h"
 #include "pico/time.h"
+#include "pico/binary_info.h"
 
 #define MICRO_SPI0_CS   1
 #define MICRO_SPI0_TX   3
@@ -29,10 +32,12 @@ bool heartbeat_callback(repeating_timer_t *rt);
 void process_cmd(char* buf, uint8_t len);
 uint8_t cton(char c);
 
-// static void read_registers(uint8_t reg, uint16_t *result);
-// static void write_register(uint8_t reg, uint8_t data);
-// static inline void cs_select();
-// static inline void cs_deselect();
+static void read_registers(uint8_t reg, uint16_t *result);
+static void write_register(uint8_t reg, uint8_t data);
+static inline void cs_select();
+static inline void cs_deselect();
+
+void set_pwm_pin(uint pin, uint freq, uint duty_c); // duty_c between 0..10000
 
 
 volatile uint8_t led_counter;
@@ -43,20 +48,40 @@ repeating_timer_t heartbeat_timer;
 int main() {
     stdio_init_all();
 
-//    spi_init(spi0, 5000000);
+    spi_init(spi0, 5000000);
 
     gpio_init(PICO_DEFAULT_LED_PIN);
     gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
 
-//    gpio_set_function(MICRO_SPI0_RX, GPIO_FUNC_SPI);
-//    gpio_set_function(MICRO_SPI0_TX, GPIO_FUNC_SPI);
-//    gpio_set_function(MICRO_SPI0_SCLK, GPIO_FUNC_SPI);
-//    bi_decl(bi_3pins_with_func(MICRO_SPI0_RX, MICRO_SPI0_TX, MICRO_SPI0_SCLK, GPIO_FUNC_SPI));
-//
-//    gpio_init(MICRO_SPI0_CS);
-//    gpio_set_dir(MICRO_SPI0_CS, GPIO_OUT);
-//    gpio_put(MICRO_SPI0_CS, 1);
-//    bi_decl(bi_1pin_with_name(MICRO_SPI0_CS, "SPI CS"));
+    gpio_init(MICRO_MOTOR_BRAKE);
+    gpio_set_dir(MICRO_MOTOR_BRAKE, GPIO_OUT);
+
+    gpio_init(MICRO_MOTOR_FGOUT);
+    gpio_set_dir(MICRO_MOTOR_FGOUT, GPIO_IN);
+
+    gpio_init(MICRO_MOTOR_nSLEEP);
+    gpio_set_dir(MICRO_MOTOR_nSLEEP, GPIO_OUT);
+    gpio_pull_up(MICRO_MOTOR_nSLEEP);
+    gpio_put(MICRO_MOTOR_nSLEEP, 1);
+
+    gpio_init(MICRO_MOTOR_nFAULT);
+    gpio_set_dir(MICRO_MOTOR_nFAULT, GPIO_IN);
+
+    gpio_init(MICRO_MOTOR_DRVOFF);
+    gpio_set_dir(MICRO_MOTOR_DRVOFF, GPIO_OUT);
+    gpio_pull_up(MICRO_MOTOR_DRVOFF);
+
+    gpio_init(MICRO_MOTOR_PWM);
+
+    gpio_set_function(MICRO_SPI0_RX, GPIO_FUNC_SPI);
+    gpio_set_function(MICRO_SPI0_TX, GPIO_FUNC_SPI);
+    gpio_set_function(MICRO_SPI0_SCLK, GPIO_FUNC_SPI);
+    bi_decl(bi_3pins_with_func(MICRO_SPI0_RX, MICRO_SPI0_TX, MICRO_SPI0_SCLK, GPIO_FUNC_SPI));
+
+    gpio_init(MICRO_SPI0_CS);
+    gpio_set_dir(MICRO_SPI0_CS, GPIO_OUT);
+    gpio_put(MICRO_SPI0_CS, 1);
+    bi_decl(bi_1pin_with_name(MICRO_SPI0_CS, "SPI CS"));
 
     gpio_put(PICO_DEFAULT_LED_PIN, 1);
 
@@ -132,7 +157,7 @@ void process_cmd(char* buf, uint8_t len) {
                     if (msb <= 0xF && lsb <= 0xF ) {
                         uint8_t reg = (msb << 4) | lsb;
                         uint16_t result = 0;
-                        // read_registers(reg, &result);
+                        read_registers(reg, &result);
                         printf("\nREG 0x%02X: 0x%04X\n", reg, result);
                         result = 0;
                     }
@@ -150,7 +175,7 @@ void process_cmd(char* buf, uint8_t len) {
                     if (msb_reg <= 0xF && lsb_reg <= 0xF && msb_dat <= 0xF && lsb_dat <= 0xF) {
                         uint8_t reg = (msb_reg << 4) | lsb_reg;
                         uint8_t data = (msb_dat << 4) | lsb_dat;
-                        // write_register(reg, data);
+                        write_register(reg, data);
                         printf("\nWrote 0x%02X to 0x%02X\n", data, reg);
                         result = 0;
                     }
@@ -169,8 +194,12 @@ void process_cmd(char* buf, uint8_t len) {
                         uint8_t duty_cycle = (msb_duty << 4) | lsb_duty;
                         if (frequency > 200) frequency = 200;
                         if (duty_cycle > 100) duty_cycle = 100;
+
                         if (motor_enabled) {
                             printf("\nOperating motor at %d kHz with a %d percent duty cycle\n", frequency, duty_cycle);
+                            duty_cycle *= 100;
+                            frequency *= 1000;
+                            set_pwm_pin(MICRO_MOTOR_PWM, frequency, duty_cycle);
                         } else {
                             printf("\nMotor is disabled! Enable motor with 'enable' to access this command!\n");
                         }
@@ -179,15 +208,13 @@ void process_cmd(char* buf, uint8_t len) {
                     break;
                 }
             }
-            case 'd': {
-                if (len >= 3 && buf[1] == 'i' && buf[2] == 'r') {
-                    printf("\nchanging motor direction!\n");
-                    result = 0;
-                }
-                break;
-            }
             case 's': {
+                printf("\nBraking!\n");
+                gpio_put(MICRO_MOTOR_BRAKE, 1);
+                sleep_ms(100);
+                gpio_put(MICRO_MOTOR_DRVOFF, 1);
                 printf("\nStopping motor!\n");
+                motor_enabled = false;
                 result = 0;
                 break;
             }
@@ -195,6 +222,9 @@ void process_cmd(char* buf, uint8_t len) {
                 if (len >= 6) {
                     if (buf[1] == 'n' && buf[2] == 'a' && buf[3] == 'b' && buf[4] == 'l' && buf[5] == 'e') {
                         printf("\nEnabling Motor!\n");
+                        motor_enabled = true;
+                        gpio_put(MICRO_MOTOR_DRVOFF, 0);
+                        gpio_put(MICRO_MOTOR_BRAKE, 0);
                         result = 0;
                     }
                 }
@@ -222,40 +252,51 @@ uint8_t cton(char c) {
     return result;
 }
 
-///* I actually fucking hate how SPI is implemented on RP2040 */
-//static inline void cs_select() {
-//    asm volatile("nop \n nop \n nop");
-//    gpio_put(PICO_DEFAULT_SPI_CSN_PIN, 0);  // Active low
-//    asm volatile("nop \n nop \n nop");
-//}
-//
-//static inline void cs_deselect() {
-//    asm volatile("nop \n nop \n nop");
-//    gpio_put(PICO_DEFAULT_SPI_CSN_PIN, 1);
-//    asm volatile("nop \n nop \n nop");
-//}
-//
-//static void write_register(uint8_t reg, uint8_t data) {
-//    uint8_t buf[2];
-//    buf[0] = reg & 0x7f;  // remove read bit as this is a write
-//    buf[1] = data;
-//    cs_select();
-//    spi_write_blocking(spi0, buf, 2);
-//    cs_deselect();
-//    sleep_ms(10);
-//}
-//
-//static void read_registers(uint8_t reg, uint16_t *result) {
-//    // For this particular device, we send the device the register we want to read
-//    // first, then subsequently read from the device. The register is auto incrementing
-//    // so we don't need to keep sending the register we want, just the first.
-//    uint8_t buf[2] = {0, 0};
-//    reg |= READ_BIT;
-//    cs_select();
-//    spi_write_blocking(spi0, &reg, 1);
-//    sleep_ms(10);
-//    spi_read_blocking(spi0, 0, buf, 2);
-//    cs_deselect();
-//    sleep_ms(10);
-//    result = (((uint16_t) buf[0] << 8) | ((uint16_t) buf[1]));
-//}
+/* I actually fucking hate how SPI is implemented on RP2040 */
+static inline void cs_select() {
+    asm volatile("nop \n nop \n nop");
+    gpio_put(MICRO_SPI0_CS, 0);  // Active low
+    asm volatile("nop \n nop \n nop");
+}
+
+static inline void cs_deselect() {
+    asm volatile("nop \n nop \n nop");
+    gpio_put(MICRO_SPI0_CS, 1);
+    asm volatile("nop \n nop \n nop");
+}
+
+static void write_register(uint8_t reg, uint8_t data) {
+    uint8_t buf[2];
+    buf[0] = reg & 0x7f;  // remove read bit as this is a write
+    buf[1] = data;
+    cs_select();
+    spi_write_blocking(spi0, buf, 2);
+    cs_deselect();
+    sleep_ms(10);
+}
+
+static void read_registers(uint8_t reg, uint16_t *result) {
+    // For this particular device, we send the device the register we want to read
+    // first, then subsequently read from the device. The register is auto incrementing
+    // so we don't need to keep sending the register we want, just the first.
+    uint8_t buf[2] = {0, 0};
+    reg |= READ_BIT;
+    cs_select();
+    spi_write_blocking(spi0, &reg, 1);
+    spi_read_blocking(spi0, 0, buf, 2);
+    cs_deselect();
+    printf("\nbuf[0]: %02X buf[1]: %02X\n", buf[0], buf[1]);
+    sleep_ms(10);
+    *result = ((((uint16_t) buf[0]) << 8) | ((uint16_t) (buf[1])));
+}
+
+void set_pwm_pin(uint pin, uint freq, uint duty_c) { // duty_c between 0..10000
+		gpio_set_function(pin, GPIO_FUNC_PWM);
+		uint slice_num = pwm_gpio_to_slice_num(pin);
+        pwm_config config = pwm_get_default_config();
+		float div = (float)clock_get_hz(clk_sys) / (freq * 10000);
+		pwm_config_set_clkdiv(&config, div);
+		pwm_config_set_wrap(&config, 10000); 
+		pwm_init(slice_num, &config, true); // start the pwm running according to the config
+		pwm_set_gpio_level(pin, duty_c); //connect the pin to the pwm engine and set the on/off level. 
+	};
