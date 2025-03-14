6c6b765a372768678802effb73842641db6984ec
J. Cian Capacci <cian.capacci@gmail.com>
2025-03-13 20:56:39 -0400

    Initial work done on converting altimeter and IMU drivers to internal I2C handler class

--- include/mid_imu.hpp
+++ include/mid_imu.hpp
@@ -5,13 +5,15 @@
 #include "hardware/gpio.h"
 #include "hardware/clocks.h"
 
+#include "sensor_i2c.hpp"
+
 #define MID_I2C_ADDR 0xD0
 
 //Hard definitions will be refactored as tables for full driver
 #define c_READ_WHOAMI 0x75
 #define v_WHOAMI_RESPONSE 0x56
 
-class mid_imu {
+class mid_imu : sensor_i2c {
 	private:
 		i2c_inst_t* inst;
 		uint8_t buffer[4];
