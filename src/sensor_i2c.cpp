80f1a419b55f1d73b016d2292f9399141fbdeabe
J. Cian Capacci <cian.capacci@gmail.com>
2025-04-06 17:59:56 -0400

    Functional refactor of high-G accelerometer driver using properly defined bitfields and constants; needs testing for scale values and offset calibration, seems like each axis has an offset ranging from +-0.3 to +-0.75 Gs

--- src/sensor_i2c.cpp
+++ src/sensor_i2c.cpp
@@ -1,20 +1,20 @@
 #include "sensor_i2c.hpp"
 
-//DEPRECATED, WILL BE REMOVED FOLLOWING REFACTOR
+//DEPRECATED - TODO: Discuss removal of SensorI2C as its scope has shrunk to almost nothing
 
-//Write uint8_t buffer to register block starting at address
+//Write uint8_t buffer to i2c with internally configured I2C instance
+void SensorI2C::write_buffer(uint8_t* buffer, size_t len, bool nostop) {
+	//Passthrough internal fields to sdk method to minimize redundant parameters
+	i2c_write_blocking(this->bus, this->addr, buffer, len, nostop);
+}
+
+//TEMPORARY, WILL BE REMOVED AFTER REFACTOR: Overloaded write_buffer to fix compilation of deprecated drivers
 void SensorI2C::write_buffer(uint8_t reg_addr, uint8_t* buffer, size_t len) {
-	//Add the register address to the outgoing I2C TX buffer before writing actual data to the TX buffer, then send
-	//i2c_write_byte_raw(this->bus, reg_addr);
-	//i2c_write_blocking(this->bus, this->addr, buffer, len, false);
-
-	//TODO: THIS IS A TEMPORARY FIX, REMOVE DYNAMIC MEMORY ALLOCATION DURING SENSOR DRIVER REFACTOR
+	//Use the botched index shift strategy with dynamic buffer allocation (ew, I know) 
 	uint8_t writeBuffer[len+1];
 	writeBuffer[0] = reg_addr;
 	for (int i = 0; i < len; i++) writeBuffer[i+1] = buffer[i];
-	printf("- SENSORI2C: Performing write operation...\n");
 	i2c_write_blocking(this->bus, this->addr, writeBuffer, len+1, false);
-	printf("- SENSORI2C: Write operation performed successfully!\n");
 }
 
 //Read register block into uint8_t buffer of parameterized len
